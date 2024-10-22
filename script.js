b78cff6eda1f7e36223dd1dcb09d5996cc001838
Kausar Khan <156500913+KausarKhan-2002@users.noreply.github.com>
2024-10-22 11:48:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 function firstWord(s) {
   // your code here
+  let space = false;
+
+  // If str is empty return str
+  if (str == "") {
+    return ""
+  }
+
+  // If str does not include space
+  for (let chr of str) {
+    if (chr == " ") {
+      space = true;
+      break;
+    }
+  }
+  if (space == false) return str;
+
+  // return first word up to the first space
+  let strTrim = str.trimStart()
+  let newStr = ""
+  for (let chr of strTrim) {
+    if (chr != " ") newStr += chr
+    else return newStr
+  }
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+// const s = prompt("Enter String:")
 alert(firstWord(s));
