4acabe3b67fe2629c0c3f30cea49485eb8fd0542
Kausar Khan <156500913+KausarKhan-2002@users.noreply.github.com>
2024-10-22 13:23:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 function firstWord(s) {
   // your code here
  if (str === '') {
-    return ''
+    return "''"
   }
 
   let indexS = str.indexOf(' ')
   
   if (indexS === -1) {
-    return str
+    return `'${str}'`
   }
 
-  return str.slice(0, indexS)
+  return `'${str.slice(0, indexS)}'`
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+// const s = prompt("Enter String:");
 alert(firstWord(s));
