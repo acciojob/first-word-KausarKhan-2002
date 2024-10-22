8449834961a070aecdb45d29350c18c3d3db7a9d
Kausar Khan <156500913+KausarKhan-2002@users.noreply.github.com>
2024-10-22 12:04:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,19 @@
 function firstWord(s) {
   // your code here
-  let space = false;
-
-  // If str is empty return str
-  if (str == "") {
-    return "''"
+ if (str === '') {
+    return ''
   }
 
-  // If str does not include space
-  for (let chr of str) {
-    if (chr == " ") {
-      space = true;
-      break;
-    }
+  let indexS = str.indexOf(' ')
+  
+  if (indexS === -1) {
+    return str
   }
-  if (space == false) return `'${str}'`;
 
-  // return first word up to the first space
-  let strTrim = str.trimStart()
-  let newStr = ""
-  for (let chr of strTrim) {
-    if (chr != " ") newStr += chr
-    else return `'${newStr}'`
-  }
+  return str.slice(0, indexS)
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:")
+// const s = prompt("Enter String:")
 alert(firstWord(s));
